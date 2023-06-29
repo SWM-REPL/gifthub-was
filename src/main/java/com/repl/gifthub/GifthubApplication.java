74c57da484e14647aec53434fdfaf8858d82933d
Jinwoo Lee <binarywooo@gmail.com>
2023-06-29 11:24:37 +0900

    chore: start Spring Project

---
+++ src/main/java/com/repl/gifthub/GifthubApplication.java
@@ -0,0 +1,13 @@
+package com.repl.gifthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifthubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repl/gifthub/GifthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repl.gifthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
