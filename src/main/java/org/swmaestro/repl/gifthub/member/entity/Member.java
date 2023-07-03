d7120ec60401f6c01115e21b4e5537b0aeac60c6
Jinwoo Lee <binarywooo@gmail.com>
2023-07-03 09:58:53 +0900

    refactor: implemented BaseTimeEntity, applied Member Entity

--- src/main/java/org/swmaestro/repl/gifthub/member/entity/Member.java
+++ src/main/java/org/swmaestro/repl/gifthub/member/entity/Member.java
@@ -2,15 +2,11 @@ package org.swmaestro.repl.gifthub.member.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
+import org.swmaestro.repl.gifthub.util.entity.BaseTimeEntity;
 
 @Entity
-@EntityListeners(AuditingEntityListener.class)
-public class Member {
+
+public class Member extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -22,14 +18,4 @@ public class Member {
 	@NotNull
 	@Column(length = 12)
 	private String nickname;
-
-	@NotNull
-	@CreatedDate
-	private LocalDateTime createdAt;
-
-	@NotNull
-	@LastModifiedDate
-	private LocalDateTime updatedAt;
-
-	private LocalDateTime deletedAt;
 }
