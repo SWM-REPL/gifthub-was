b8f8b883c38fbf41cac42ef85c746bc9bba33672
Jinwoo Lee <binarywooo@gmail.com>
2023-07-04 11:29:55 +0900

    feat: implement consturctor and builder to Member

--- src/main/java/org/swmaestro/repl/gifthub/member/entity/Member.java
+++ src/main/java/org/swmaestro/repl/gifthub/member/entity/Member.java
@@ -2,14 +2,21 @@ package org.swmaestro.repl.gifthub.member.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.swmaestro.repl.gifthub.util.entity.BaseTimeEntity;
 
 @Entity
-
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotNull
+	@Column(length = 60)
+	private String username;
 
 	@NotNull
 	@Column(length = 60)
@@ -18,4 +25,12 @@ public class Member extends BaseTimeEntity {
 	@NotNull
 	@Column(length = 12)
 	private String nickname;
+
+	@Builder
+	public Member(Long id, @NotNull String username, @NotNull String password, @NotNull String nickname) {
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.nickname = nickname;
+	}
 }
