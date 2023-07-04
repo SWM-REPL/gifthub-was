ec5f3529ec8c96339813851f192b7a12722bcfc4
Jinwoo Lee <binarywooo@gmail.com>
2023-07-05 06:17:45 +0900

    feat: implement passwordEncryption test

--- src/test/java/org/swmaestro/repl/gifthub/member/service/MemberServiceTest.java
+++ src/test/java/org/swmaestro/repl/gifthub/member/service/MemberServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.swmaestro.repl.gifthub.member.entity.Member;
 import org.swmaestro.repl.gifthub.member.repository.SpringDataJpaMemberRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
@@ -41,5 +42,23 @@ class MemberServiceTest {
 
 		// then
 		verify(memberRepository).save(member);
+	}
+
+	@Test
+	void passwordEncryption() {
+		// given
+		String testPassword = "abc123";
+		Member member = Member.builder()
+			.id(Long.valueOf(1))
+			.username("jinlee1703")
+			.password(testPassword)
+			.nickname("이진우")
+			.build();
+
+		// when
+		memberService.signUp(member);
+
+		// then
+		assertThat(memberRepository.findById(Long.valueOf(1)).get().getPassword()).isNotEqualTo(testPassword);
 	}
 }
