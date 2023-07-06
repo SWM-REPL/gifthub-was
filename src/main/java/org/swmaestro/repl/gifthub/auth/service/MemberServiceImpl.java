82ec559e6cb43aebc783fe2cddce6f62288d4689
inh2613 <inh2613@naver.com>
2023-07-06 16:37:35 +0900

    feat: implement Read MemberService

--- src/main/java/org/swmaestro/repl/gifthub/auth/service/MemberServiceImpl.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/MemberServiceImpl.java
@@ -13,84 +13,88 @@ import java.util.regex.Pattern;
 
 @Service
 public class MemberServiceImpl implements MemberService {
-	private final SpringDataJpaMemberRepository memberRepository;
-	private final PasswordEncoder passwordEncoder;
+    private final SpringDataJpaMemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
-	@Autowired
-	public MemberServiceImpl(SpringDataJpaMemberRepository memberRepository, PasswordEncoder passwordEncoder) {
-		this.memberRepository = memberRepository;
-		this.passwordEncoder = passwordEncoder;
-	}
+    @Autowired
+    public MemberServiceImpl(SpringDataJpaMemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
-	public Member passwordEncryption(Member member) {
-		return Member.builder()
-			.username(member.getUsername())
-			.password(passwordEncoder.encode(member.getPassword()))
-			.nickname(member.getNickname())
-			.build();
-	}
+    public Member passwordEncryption(Member member) {
+        return Member.builder()
+                .username(member.getUsername())
+                .password(passwordEncoder.encode(member.getPassword()))
+                .nickname(member.getNickname())
+                .build();
+    }
 
-	@Override
-	public Long create(SignUpDto signUpDTO) {
-		if (isDuplicateUsername(signUpDTO.getUsername()) ||
-			isDuplicateNickname(signUpDTO.getNickname()) ||
-			!isValidatePassword(signUpDTO.getPassword())) {
-			return -1L;
-		}
+    @Override
+    public Long create(SignUpDto signUpDTO) {
+        if (isDuplicateUsername(signUpDTO.getUsername()) ||
+                isDuplicateNickname(signUpDTO.getNickname()) ||
+                !isValidatePassword(signUpDTO.getPassword())) {
+            return -1L;
+        }
 
-		Member member = convertSignUpDTOtoMember(signUpDTO);
-		Member encodedMember = passwordEncryption(member);
+        Member member = convertSignUpDTOtoMember(signUpDTO);
+        Member encodedMember = passwordEncryption(member);
 
-		memberRepository.save(encodedMember);
-		return encodedMember.getId();
-	}
+        memberRepository.save(encodedMember);
+        return encodedMember.getId();
+    }
 
-	public Member convertSignUpDTOtoMember(SignUpDto signUpDTO) {
-		return Member.builder()
-			.username(signUpDTO.getUsername())
-			.password(signUpDTO.getPassword())
-			.nickname(signUpDTO.getNickname())
-			.build();
-	}
+    public Member convertSignUpDTOtoMember(SignUpDto signUpDTO) {
+        return Member.builder()
+                .username(signUpDTO.getUsername())
+                .password(signUpDTO.getPassword())
+                .nickname(signUpDTO.getNickname())
+                .build();
+    }
 
-	public boolean isDuplicateUsername(String username) {
-		return memberRepository.findByUsername(username) != null;
-	}
+    public boolean isDuplicateUsername(String username) {
+        return memberRepository.findByUsername(username) != null;
+    }
 
-	public boolean isDuplicateNickname(String nickname) {
-		return memberRepository.findByNickname(nickname) != null;
-	}
+    public boolean isDuplicateNickname(String nickname) {
+        return memberRepository.findByNickname(nickname) != null;
+    }
 
-	public boolean isValidatePassword(String password) {
-		String regex = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=_\\-!]).{8,64}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(password);
+    public boolean isValidatePassword(String password) {
+        String regex = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@#$%^&+=_\\-!]).{8,64}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
 
-		return matcher.matches();
-	}
+        return matcher.matches();
+    }
 
-	@Override
-	public Member read(Long id) {
-		return null;
-	}
+    @Override
+    public Member read(SignUpDto signUpDto) {
+        Member member = memberRepository.findByUsername(signUpDto.getUsername());
+        if (member == null) {
+            return null;
+        }
+        return member;
+    }
 
-	@Override
-	public int count() {
-		return 0;
-	}
+    @Override
+    public int count() {
+        return (int) memberRepository.count();
+    }
 
-	@Override
-	public List<Member> list() {
-		return null;
-	}
+    @Override
+    public List<Member> list() {
+        return memberRepository.findAll();
+    }
 
-	@Override
-	public Long update(Long id, Member member) {
-		return null;
-	}
+    @Override
+    public Long update(Long id, Member member) {
+        return null;
+    }
 
-	@Override
-	public Long delete(Long id) {
-		return null;
-	}
+    @Override
+    public Long delete(Long id) {
+        return null;
+    }
 }
