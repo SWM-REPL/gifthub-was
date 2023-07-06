6213f3bd791184427bfd752a182074d5b02b3780
inh2613 <inh2613@naver.com>
2023-07-06 19:18:42 +0900

    feat: implement Verify Password Service

---
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
@@ -0,0 +1,28 @@
+package org.swmaestro.repl.gifthub.auth.service;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.swmaestro.repl.gifthub.auth.dto.SignUpDto;
+import org.swmaestro.repl.gifthub.auth.entity.Member;
+import org.swmaestro.repl.gifthub.auth.repository.SpringDataJpaMemberRepository;
+
+public class AuthServiceImpl implements AuthService {
+    private final SpringDataJpaMemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(SpringDataJpaMemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public SignUpDto verifyPassword(SignUpDto signUpDto) {
+        Member member = memberRepository.findByUsername(signUpDto.getUsername());
+
+        if (member != null && passwordEncoder.matches(signUpDto.getPassword(), member.getPassword())) {
+            return SignUpDto.builder()
+                    .username(member.getUsername())
+                    .password(member.getPassword())
+                    .build();
+        }
+        return null;
+    }
+}
