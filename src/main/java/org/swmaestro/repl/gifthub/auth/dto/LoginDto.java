0e9c126f2edef03caa872406730137f464737275
inh2613 <inh2613@naver.com>
2023-07-07 16:58:33 +0900

    feat: implement Login API

---
+++ src/main/java/org/swmaestro/repl/gifthub/auth/dto/LoginDto.java
@@ -0,0 +1,20 @@
+package org.swmaestro.repl.gifthub.auth.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class LoginDto {
+	@NotNull
+	@Size(min = 4, max = 60)
+	private String username;
+
+	@NotNull
+	@Size(min = 8, max = 64)
+	private String password;
+}
--- src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
@@ -1,27 +1,26 @@
 package org.swmaestro.repl.gifthub.auth.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.swmaestro.repl.gifthub.auth.dto.SignUpDto;
+import org.springframework.stereotype.Service;
+import org.swmaestro.repl.gifthub.auth.dto.LoginDto;
 import org.swmaestro.repl.gifthub.auth.entity.Member;
 import org.swmaestro.repl.gifthub.auth.repository.SpringDataJpaMemberRepository;
 
+@Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 	private final SpringDataJpaMemberRepository memberRepository;
 	private final PasswordEncoder passwordEncoder;
+	
+	public LoginDto verifyPassword(LoginDto loginDto) {
+		Member member = memberRepository.findByUsername(loginDto.getUsername());
 
-	public AuthServiceImpl(SpringDataJpaMemberRepository memberRepository, PasswordEncoder passwordEncoder) {
-		this.memberRepository = memberRepository;
-		this.passwordEncoder = passwordEncoder;
-	}
-
-	public SignUpDto verifyPassword(SignUpDto signUpDto) {
-		Member member = memberRepository.findByUsername(signUpDto.getUsername());
-
-		if (member != null && passwordEncoder.matches(signUpDto.getPassword(), member.getPassword())) {
-			return SignUpDto.builder()
-				.username(member.getUsername())
-				.password(member.getPassword())
-				.build();
+		if (member != null && passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
+			return LoginDto.builder()
+					.username(member.getUsername())
+					.password(member.getPassword())
+					.build();
 		}
 		return null;
 	}
