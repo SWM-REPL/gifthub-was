1e0d8d8c125e168864aef1187f28c7ae6cdd254c
Jinwoo Lee <binarywooo@gmail.com>
2023-07-07 15:04:52 +0900

    feat: implement jwt validate test

--- src/test/java/org/swmaestro/repl/gifthub/util/JwtUtilTest.java
+++ src/test/java/org/swmaestro/repl/gifthub/util/JwtUtilTest.java
@@ -6,8 +6,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class JwtUtilTest {
@@ -17,10 +17,30 @@ class JwtUtilTest {
 	@Test
 	@DisplayName("generateToken test")
 	void genderateToken() {
+		// given
 		String username = "jinwoolee";
 
+		// when
 		jwtUtil.generateToken(username);
 
+		// then
 		verify(jwtUtil, times(1)).generateToken(username);
+	}
+
+	@Test
+	@DisplayName("validateToken test")
+	void validateToken() {
+		// given
+		String username = "jinwoolee";
+
+		// when
+		when(jwtUtil.generateToken(anyString())).thenReturn("myawesomejwt");
+		when(jwtUtil.validateToken(anyString())).thenReturn(true);
+
+		String myJwtToken = jwtUtil.generateToken(username);
+
+		// then
+		assertTrue(jwtUtil.validateToken(myJwtToken));
+		verify(jwtUtil, times(1)).validateToken(myJwtToken);
 	}
 }
