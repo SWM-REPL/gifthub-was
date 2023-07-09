f4ad4c8ea41b9651c64c19f159ec13d905168b5b
Jinwoo Lee <binarywooo@gmail.com>
2023-07-09 14:31:58 +0900

    chore: change JwtAuthenticationFilter's path

--- src/main/java/org/swmaestro/repl/gifthub/filter/JwtAuthenticationFilter.java
+++ src/main/java/org/swmaestro/repl/gifthub/filter/JwtAuthenticationFilter.java
@@ -1,4 +1,4 @@
-package org.swmaestro.repl.gifthub.util.filter;
+package org.swmaestro.repl.gifthub.filter;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
--- src/main/java/org/swmaestro/repl/gifthub/util/JwtProvider.java
+++ src/main/java/org/swmaestro/repl/gifthub/util/JwtProvider.java
@@ -10,7 +10,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
-import org.swmaestro.repl.gifthub.auth.service.JpaUserDetailsService;
+import org.swmaestro.repl.gifthub.security.JpaUserDetailsService;
 
 import java.util.Date;
 
