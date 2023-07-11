9e0af8d68b65883a5dcb3c21f3957048a56a76b6
inh2613 <inh2613@naver.com>
2023-07-11 20:22:08 +0900

    fix: Add Exception Handling to signIn from AuthServiceImpl

--- src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
@@ -7,6 +7,8 @@ import org.swmaestro.repl.gifthub.auth.dto.SignInDto;
 import org.swmaestro.repl.gifthub.auth.dto.TokenDto;
 import org.swmaestro.repl.gifthub.auth.entity.Member;
 import org.swmaestro.repl.gifthub.auth.repository.MemberRepository;
+import org.swmaestro.repl.gifthub.exception.BusinessException;
+import org.swmaestro.repl.gifthub.exception.ErrorCode;
 import org.swmaestro.repl.gifthub.util.JwtProvider;
 
 @Service
@@ -19,20 +21,23 @@ public class AuthServiceImpl implements AuthService {
 
 	public TokenDto signIn(SignInDto loginDto) {
 		Member member = memberRepository.findByUsername(loginDto.getUsername());
-
-		if (member != null && passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
-			String accessToken = jwtProvider.generateToken(member.getUsername());
-			String refreshToken = jwtProvider.generateRefreshToken(member.getUsername());
-
-			TokenDto tokenDto = TokenDto.builder()
-					.accessToken(accessToken)
-					.refreshToken(refreshToken)
-					.build();
-
-			refreshTokenService.storeRefreshToken(tokenDto, member.getUsername());
-
-			return tokenDto;
+		if (member == null) {
+			throw new BusinessException("존재하지 않는 아이디입니다.", ErrorCode.INVALID_INPUT_VALUE);
 		}
-		return null;
+		if (!passwordEncoder.matches(loginDto.getPassword(), member.getPassword())) {
+			throw new BusinessException("비밀번호가 일치하지 않습니다.", ErrorCode.INVALID_INPUT_VALUE);
+		}
+		String accessToken = jwtProvider.generateToken(member.getUsername());
+		String refreshToken = jwtProvider.generateRefreshToken(member.getUsername());
+
+		TokenDto tokenDto = TokenDto.builder()
+				.accessToken(accessToken)
+				.refreshToken(refreshToken)
+				.build();
+
+		refreshTokenService.storeRefreshToken(tokenDto, member.getUsername());
+
+		return tokenDto;
+
 	}
 }
