03e715e9d60fc4019c0d7288d10ff404adfa0fd6
Jinwoo Lee <binarywooo@gmail.com>
2023-07-12 01:28:46 +0900

    feat: add annotation for swagger to AuthController

--- src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package org.swmaestro.repl.gifthub.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.swmaestro.repl.gifthub.auth.dto.SignInDto;
@@ -13,6 +15,7 @@ import org.swmaestro.repl.gifthub.util.JwtProvider;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Auth", description = "사용자 인증 관련 API")
 public class AuthController {
 	private final MemberService memberService;
 	private final AuthService authService;
@@ -20,18 +23,21 @@ public class AuthController {
 	private final JwtProvider jwtProvider;
 
 	@PostMapping("/sign-up")
+	@Operation(summary = "회원가입 메서드", description = "사용자가 회원가입을 하기 위한 메서드입니다.")
 	public TokenDto signUp(@RequestBody SignUpDto signUpDto) {
 		TokenDto tokenDto = memberService.create(signUpDto);
 		return tokenDto;
 	}
 
 	@PostMapping("/sign-in")
+	@Operation(summary = "로그인 메서드", description = "사용자가 로그인을 하기 위한 메서드입니다.")
 	public TokenDto signIn(@RequestBody SignInDto loginDto) {
 		TokenDto tokenDto = authService.signIn(loginDto);
 		return tokenDto;
 	}
 
 	@PostMapping("/refresh")
+	@Operation(summary = "Refresh Token 재발급 메서드", description = "Refresh Token을 재발급 받기 위한 메서드입니다.")
 	public TokenDto validateRefreshToken(@RequestHeader("Authorization") String refreshToken) {
 		String newAccessToken = refreshTokenService.createNewAccessTokenByValidateRefreshToken(refreshToken);
 		String newRefreshToken = refreshTokenService.createNewRefreshTokenByValidateRefreshToken(refreshToken);
