fc755d937758f966b87ea1854d45937adec2c9f7
Jinwoo Lee <binarywooo@gmail.com>
2023-07-12 01:27:27 +0900

    refactor: add prefix to AuthController

--- src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
@@ -1,10 +1,7 @@
 package org.swmaestro.repl.gifthub.auth.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.swmaestro.repl.gifthub.auth.dto.SignInDto;
 import org.swmaestro.repl.gifthub.auth.dto.SignUpDto;
 import org.swmaestro.repl.gifthub.auth.dto.TokenDto;
@@ -14,6 +11,7 @@ import org.swmaestro.repl.gifthub.auth.service.RefreshTokenService;
 import org.swmaestro.repl.gifthub.util.JwtProvider;
 
 @RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 	private final MemberService memberService;
@@ -21,27 +19,27 @@ public class AuthController {
 	private final RefreshTokenService refreshTokenService;
 	private final JwtProvider jwtProvider;
 
-	@PostMapping("/auth/sign-up")
+	@PostMapping("/sign-up")
 	public TokenDto signUp(@RequestBody SignUpDto signUpDto) {
 		TokenDto tokenDto = memberService.create(signUpDto);
 		return tokenDto;
 	}
 
-	@PostMapping("/auth/sign-in")
+	@PostMapping("/sign-in")
 	public TokenDto signIn(@RequestBody SignInDto loginDto) {
 		TokenDto tokenDto = authService.signIn(loginDto);
 		return tokenDto;
 	}
 
-	@PostMapping("/auth/refresh")
+	@PostMapping("/refresh")
 	public TokenDto validateRefreshToken(@RequestHeader("Authorization") String refreshToken) {
 		String newAccessToken = refreshTokenService.createNewAccessTokenByValidateRefreshToken(refreshToken);
 		String newRefreshToken = refreshTokenService.createNewRefreshTokenByValidateRefreshToken(refreshToken);
 
 		TokenDto tokenDto = TokenDto.builder()
-				.accessToken(newAccessToken)
-				.refreshToken(newRefreshToken)
-				.build();
+			.accessToken(newAccessToken)
+			.refreshToken(newRefreshToken)
+			.build();
 
 		refreshToken = refreshToken.substring(7);
 		refreshTokenService.storeRefreshToken(tokenDto, jwtProvider.getUsername(refreshToken));
