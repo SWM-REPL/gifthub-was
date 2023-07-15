6459bc4b758518f44a4f1ca74932f0a09842f2ec
Jinwoo Lee <binarywooo@gmail.com>
2023-07-15 18:50:57 +0900

    feat: implement naver-service for oauth

--- src/main/java/org/swmaestro/repl/gifthub/auth/service/NaverService.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/NaverService.java
@@ -1,10 +1,20 @@
 package org.swmaestro.repl.gifthub.auth.service;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
+import org.swmaestro.repl.gifthub.auth.dto.TokenDto;
+import org.swmaestro.repl.gifthub.auth.entity.Member;
 import org.swmaestro.repl.gifthub.auth.repository.MemberRepository;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 @PropertySource("classpath:application.yml")
@@ -16,6 +26,10 @@ public class NaverService {
 	private final String responseType;
 	private final String authorizationUri;
 	private final String redirectUri;
+	private final String clientSecret;
+	private final String tokenUri;
+	private final String userInfoUri;
+	private final JsonParser parser = new JsonParser();
 
 
 	public NaverService(MemberService memberService,
@@ -24,7 +38,10 @@ public class NaverService {
 	                    @Value("${naver.state}") String state,
 	                    @Value("${naver.response-type}") String responseType,
 	                    @Value("${naver.authorization-uri}") String authorizationUri,
-	                    @Value("${naver.redirect-uri}") String redirectUri) {
+	                    @Value("${naver.redirect-uri}") String redirectUri,
+	                    @Value("${naver.client-secret}") String clientSecret,
+	                    @Value("${naver.token-uri}") String tokenUri,
+	                    @Value("${naver.user-info-uri}") String userInfoUri) {
 		this.memberService = memberService;
 		this.memberRepository = memberRepository;
 		this.clientId = clientId;
@@ -32,6 +49,9 @@ public class NaverService {
 		this.responseType = responseType;
 		this.authorizationUri = authorizationUri;
 		this.redirectUri = redirectUri;
+		this.clientSecret = clientSecret;
+		this.tokenUri = tokenUri;
+		this.userInfoUri = userInfoUri;
 	}
 
 	public String getAuthorizationUrl() {
@@ -42,5 +62,82 @@ public class NaverService {
 			.queryParam("redirect_uri", redirectUri)
 			.queryParam("state", state)
 			.build().toString();
+	}
+
+	public TokenDto getNaverToken(String type, String code) throws IOException {
+		URL url = new URL(UriComponentsBuilder
+			.fromUriString(tokenUri)
+			.queryParam("grant_type", "authorization_code")
+			.queryParam("client_id", clientId)
+			.queryParam("client_secret", clientSecret)
+			.queryParam("code", code)
+			.build()
+			.toString());
+
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+
+		int responseCode = con.getResponseCode();
+		BufferedReader br;
+
+		if (responseCode == 200) { // 정상 호출
+			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		} else {  // 에러 발생
+			br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+		}
+
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = br.readLine()) != null) {
+			response.append(inputLine);
+		}
+
+		br.close();
+
+		JsonElement element = parser.parse(response.toString());
+
+		return TokenDto.builder()
+			.accessToken(element.getAsJsonObject().get("access_token").getAsString())
+			.refreshToken(element.getAsJsonObject().get("refresh_token").getAsString())
+			.build();
+	}
+
+	public Member getNaverUserByToken(TokenDto token) throws IOException {
+		String accessToken = token.getAccessToken();
+
+		URL url = new URL(userInfoUri);
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.setRequestMethod("GET");
+		con.setRequestProperty("Authorization", "Bearer " + accessToken);
+
+		int responseCode = con.getResponseCode();
+		BufferedReader br;
+
+		if (responseCode == 200) { // 정상 호출
+			br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		} else {  // 에러 발생
+			br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+		}
+
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+		while ((inputLine = br.readLine()) != null) {
+			response.append(inputLine);
+		}
+
+		br.close();
+
+		JsonElement element = parser.parse(response.toString());
+
+		return Member.builder()
+			.username(element.getAsJsonObject().get("response").getAsJsonObject().get("email").getAsString())
+			.nickname(element.getAsJsonObject().get("response").getAsJsonObject().get("nickname").getAsString())
+			.build();
+	}
+
+	public void saveNaverUser(Member member) {
+		if (!memberService.isDuplicateUsername(member.getUsername())) {
+			memberRepository.save(member);
+		}
 	}
 }
