2b0090bf3ee2a63a9f1748eaad47eaea41feabee
Jinwoo Lee <binarywooo@gmail.com>
2023-07-16 09:16:02 +0900

    feat: implement create private-key method to apple-service

--- src/main/java/org/swmaestro/repl/gifthub/auth/service/AppleService.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/AppleService.java
@@ -1,14 +1,15 @@
 package org.swmaestro.repl.gifthub.auth.service;
 
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 import org.swmaestro.repl.gifthub.auth.repository.MemberRepository;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.security.PrivateKey;
 
 @Service
 @PropertySource("classpath:application.yml")
@@ -47,5 +48,13 @@ public class AppleService {
 			stringBuilder.append("\n");
 		}
 		return stringBuilder.toString();
+	}
+
+	public PrivateKey craetePrivateKey(String keyPath) throws IOException {
+		Reader reader = new StringReader(keyPath);
+		PEMParser pemParser = new PEMParser(reader);
+		JcaPEMKeyConverter jcaPEMKeyConverter = new JcaPEMKeyConverter();
+		PrivateKeyInfo privateKeyInfo = (PrivateKeyInfo) pemParser.readObject();
+		return jcaPEMKeyConverter.getPrivateKey(privateKeyInfo);
 	}
 }
