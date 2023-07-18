3e07be81a41fc533510becfba9b24aff95688ae1
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-18 23:44:58 +0900

    chore: move base-time-entity

--- src/main/java/org/swmaestro/repl/gifthub/auth/entity/RefreshToken.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/entity/RefreshToken.java
@@ -1,11 +1,16 @@
 package org.swmaestro.repl.gifthub.auth.entity;
 
-import jakarta.persistence.*;
+import org.swmaestro.repl.gifthub.util.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 
 @Entity
 @Getter
