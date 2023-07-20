3826d6b0ef549d93f056e08f648139a784b3df34
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-21 04:08:34 +0900

    feat: implement read method from brand-service

---
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/service/BrandService.java
@@ -0,0 +1,17 @@
+package org.swmaestro.repl.gifthub.vouchers.service;
+
+import org.springframework.stereotype.Service;
+import org.swmaestro.repl.gifthub.vouchers.entity.Brand;
+import org.swmaestro.repl.gifthub.vouchers.repository.BrandRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class BrandService {
+	private final BrandRepository brandRepository;
+
+	public Brand read(String brandName) {
+		return brandRepository.findByBrandName(brandName);
+	}
+}
