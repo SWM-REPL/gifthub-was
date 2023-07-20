f680ddf06eff5e16458f7821d112566764520c49
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-21 04:10:32 +0900

    feat: implement read method from product-service

---
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/service/ProductService.java
@@ -0,0 +1,17 @@
+package org.swmaestro.repl.gifthub.vouchers.service;
+
+import org.springframework.stereotype.Service;
+import org.swmaestro.repl.gifthub.vouchers.entity.Product;
+import org.swmaestro.repl.gifthub.vouchers.repository.ProductRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+	private final ProductRepository productRepository;
+
+	public Product read(String productName) {
+		return productRepository.findByProductName(productName);
+	}
+}
