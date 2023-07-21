cb3671394a136a53acba9b5a26b966cec907e18a
inh2613 <inh2613@naver.com>
2023-07-21 17:57:18 +0900

    fix: add username in VoucherDto

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/dto/VoucherDto.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/dto/VoucherDto.java
@@ -1,11 +1,10 @@
 package org.swmaestro.repl.gifthub.vouchers.dto;
 
-import org.springframework.web.multipart.MultipartFile;
-
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
@@ -17,4 +16,5 @@ public class VoucherDto {
 	private String productName;
 	private String brandName;
 	private MultipartFile imageFile;
+	private String username;
 }
