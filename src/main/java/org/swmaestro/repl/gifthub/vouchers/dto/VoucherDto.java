9e4402482aceacec004a653923bc4e9ce7868d13
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-22 00:56:05 +0900

    fix: modify voucher-save logic

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/dto/VoucherDto.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/dto/VoucherDto.java
@@ -1,19 +1,36 @@
 package org.swmaestro.repl.gifthub.vouchers.dto;
 
+import java.io.Serializable;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
 @ToString
-@Builder
-public class VoucherDto {
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class VoucherDto implements Serializable {
 	private String barcode;
 	private String expiresAt;
 	private String productName;
 	private String brandName;
 	private MultipartFile imageFile;
+	private String username;
+
+	@Builder
+	public VoucherDto(String barcode, String expiresAt, String productName, String brandName, MultipartFile imageFile,
+		String username) {
+		this.barcode = barcode;
+		this.expiresAt = expiresAt;
+		this.productName = productName;
+		this.brandName = brandName;
+		this.imageFile = imageFile;
+		this.username = username;
+	}
 }
