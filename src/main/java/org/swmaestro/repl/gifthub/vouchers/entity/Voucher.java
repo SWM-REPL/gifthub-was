1a8e258ffeeef0d71176eb6dc44e2151613687a1
inh2613 <inh2613@naver.com>
2023-07-21 18:03:35 +0900

    fix: add member in Voucher

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/entity/Voucher.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/entity/Voucher.java
@@ -1,20 +1,14 @@
 package org.swmaestro.repl.gifthub.vouchers.entity;
 
-import java.time.LocalDateTime;
-
-import org.swmaestro.repl.gifthub.util.BaseTimeEntity;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.swmaestro.repl.gifthub.auth.entity.Member;
+import org.swmaestro.repl.gifthub.util.BaseTimeEntity;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -44,14 +38,19 @@ public class Voucher extends BaseTimeEntity {
 	@Column(length = 100)
 	private String imageUrl;
 
+	@ManyToOne
+	@JoinColumn(name = "member_id", nullable = false)
+	private Member member;
+
 	@Builder
 	public Voucher(Long id, Brand brand, Product product, String barcode, int balance, LocalDateTime expiresAt,
-		String imageUrl) {
+	               String imageUrl, Member member) {
 		this.id = id;
 		this.brand = brand;
 		this.product = product;
 		this.barcode = barcode;
 		this.balance = balance;
 		this.expiresAt = expiresAt;
+		this.member = member;
 	}
 }
