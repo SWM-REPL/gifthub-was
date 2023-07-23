f0cc4df31be36e9b425f60a1119ddd07050a5ba8
inh2613 <inh2613@naver.com>
2023-07-23 22:00:07 +0900

    feat: implement Voucher read API in VoucherController

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherController.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherController.java
@@ -1,25 +1,21 @@
 package org.swmaestro.repl.gifthub.vouchers.controller;
 
-import java.io.IOException;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.swmaestro.repl.gifthub.util.JwtProvider;
 import org.swmaestro.repl.gifthub.vouchers.dto.S3FileDto;
+import org.swmaestro.repl.gifthub.vouchers.dto.VoucherReadResponseDto;
 import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveRequestDto;
 import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveResponseDto;
 import org.swmaestro.repl.gifthub.vouchers.service.StorageService;
 import org.swmaestro.repl.gifthub.vouchers.service.VoucherService;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/vouchers")
@@ -40,10 +36,15 @@ public class VoucherController {
 
 	@PostMapping
 	@Operation(summary = "Voucher 등록 메서드", description = "클라이언트에서 요청한 기프티콘 정보를 저장하기 위한 메서드입니다.")
-	public VoucherSaveResponseDto saveVoucher(HttpServletRequest request,
-		@RequestBody VoucherSaveRequestDto voucherSaveRequestDto) throws
-		IOException {
+	public VoucherSaveResponseDto saveVoucher(HttpServletRequest request, @RequestBody VoucherSaveRequestDto voucherSaveRequestDto) throws
+			IOException {
 		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
 		return voucherService.save(username, voucherSaveRequestDto);
+	}
+
+	@GetMapping("/{voucherId}")
+	@Operation(summary = "Voucher 상세 조회 메서드", description = "클라이언트에서 요청한 기프티콘 상세 정보를 조회하기 위한 메서드입니다.")
+	public VoucherReadResponseDto readVoucher(@PathVariable Long voucherId) throws IOException {
+		return voucherService.read(voucherId);
 	}
 }
--- src/main/java/org/swmaestro/repl/gifthub/vouchers/service/VoucherService.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/service/VoucherService.java
@@ -1,20 +1,20 @@
 package org.swmaestro.repl.gifthub.vouchers.service;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.swmaestro.repl.gifthub.auth.service.MemberService;
 import org.swmaestro.repl.gifthub.exception.BusinessException;
 import org.swmaestro.repl.gifthub.exception.ErrorCode;
 import org.swmaestro.repl.gifthub.util.DateConverter;
+import org.swmaestro.repl.gifthub.vouchers.dto.VoucherReadResponseDto;
 import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveRequestDto;
 import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveResponseDto;
 import org.swmaestro.repl.gifthub.vouchers.entity.Voucher;
 import org.swmaestro.repl.gifthub.vouchers.repository.VoucherRepository;
 
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -29,31 +29,38 @@ public class VoucherService {
 		기프티콘 저장 메서드
 	 */
 	public VoucherSaveResponseDto save(String username, VoucherSaveRequestDto voucherSaveRequestDto) throws
-		IOException {
+			IOException {
 		Voucher voucher = Voucher.builder()
-			.brand(brandService.read(voucherSaveRequestDto.getBrandName()))
-			.product(productService.read(voucherSaveRequestDto.getProductName()))
-			.barcode(voucherSaveRequestDto.getBarcode())
-			.expiresAt(DateConverter.stringToLocalDate(voucherSaveRequestDto.getExpiresAt()))
-			.imageUrl(voucherSaveRequestDto.getImageUrl())
-			.member(memberService.read(username))
-			.build();
+				.brand(brandService.read(voucherSaveRequestDto.getBrandName()))
+				.product(productService.read(voucherSaveRequestDto.getProductName()))
+				.barcode(voucherSaveRequestDto.getBarcode())
+				.expiresAt(DateConverter.stringToLocalDate(voucherSaveRequestDto.getExpiresAt()))
+				.imageUrl(voucherSaveRequestDto.getImageUrl())
+				.member(memberService.read(username))
+				.build();
 
 		return VoucherSaveResponseDto.builder()
-			.id(voucherRepository.save(voucher).getId())
-			.build();
+				.id(voucherRepository.save(voucher).getId())
+				.build();
 	}
 
 	/*
 	기프티콘 상세 조회 메서드
 	 */
-	public Optional<Voucher> read(Long id) {
+	public VoucherReadResponseDto read(Long id) {
 		Optional<Voucher> voucher = voucherRepository.findById(id);
 
 		if (voucher == null) {
 			throw new BusinessException("존재하지 않는 상품권 입니다.", ErrorCode.NOT_FOUND_RESOURCE);
 		}
-		return voucher;
+		VoucherReadResponseDto voucherReadResponseDto = VoucherReadResponseDto.builder()
+				.id(voucher.get().getId())
+				.barcode(voucher.get().getBarcode())
+				.expiresAt(voucher.get().getExpiresAt().toString())
+				.product(voucher.get().getProduct())
+				.username(voucher.get().getMember().getUsername())
+				.build();
+		return voucherReadResponseDto;
 	}
 
 	/*
