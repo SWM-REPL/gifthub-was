5095adeb64142d325b42c3427604cf4fb317dea0
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-23 13:28:19 +0900

    feat: implement voucher-save api

--- src/main/java/org/swmaestro/repl/gifthub/util/JwtProvider.java
+++ src/main/java/org/swmaestro/repl/gifthub/util/JwtProvider.java
@@ -1,9 +1,9 @@
 package org.swmaestro.repl.gifthub.util;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import io.jsonwebtoken.Jwts;
-import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,9 +13,10 @@ import org.springframework.stereotype.Component;
 import org.swmaestro.repl.gifthub.auth.repository.RefreshTokenRepository;
 import org.swmaestro.repl.gifthub.security.JpaUserDetailsService;
 
-import java.time.Instant;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Component
 @PropertySource("classpath:application.yml")
@@ -26,7 +27,9 @@ public class JwtProvider {
 	private final JpaUserDetailsService userDetailsService;
 	private final RefreshTokenRepository refreshTokenRepository;
 
-	public JwtProvider(@Value("${jwt.secret-key}") String secretKey, @Value("${jwt.expiration-time}") long expiration, @Value("${issuer}") String issuer, JpaUserDetailsService userDetailsService, RefreshTokenRepository refreshTokenRepository) {
+	public JwtProvider(@Value("${jwt.secret-key}") String secretKey, @Value("${jwt.expiration-time}") long expiration,
+		@Value("${issuer}") String issuer, JpaUserDetailsService userDetailsService,
+		RefreshTokenRepository refreshTokenRepository) {
 		this.secretKey = secretKey;
 		this.expiration = expiration;
 		this.issuer = issuer;
@@ -42,12 +45,12 @@ public class JwtProvider {
 	 */
 	public String generateToken(String username) {
 		return io.jsonwebtoken.Jwts.builder()
-				.setSubject(username)
-				.setIssuer(issuer)
-				.setIssuedAt(new java.util.Date(System.currentTimeMillis()))
-				.setExpiration(new java.util.Date(System.currentTimeMillis() + expiration))
-				.signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, secretKey.getBytes())
-				.compact();
+			.setSubject(username)
+			.setIssuer(issuer)
+			.setIssuedAt(new java.util.Date(System.currentTimeMillis()))
+			.setExpiration(new java.util.Date(System.currentTimeMillis() + expiration))
+			.signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, secretKey.getBytes())
+			.compact();
 	}
 
 	/**
@@ -101,11 +104,11 @@ public class JwtProvider {
 	 */
 	public String getUsername(String token) {
 		return Jwts.parserBuilder()
-				.setSigningKey(secretKey.getBytes())
-				.build()
-				.parseClaimsJws(token)
-				.getBody()
-				.getSubject();
+			.setSigningKey(secretKey.getBytes())
+			.build()
+			.parseClaimsJws(token)
+			.getBody()
+			.getSubject();
 	}
 
 	/**
@@ -116,12 +119,12 @@ public class JwtProvider {
 	 */
 	public String generateRefreshToken(String username) {
 		return io.jsonwebtoken.Jwts.builder()
-				.setSubject(username)
-				.setIssuer(issuer)
-				.setIssuedAt(new java.util.Date(System.currentTimeMillis()))
-				.setExpiration(Date.from(Instant.now().plus(15, ChronoUnit.DAYS)))
-				.signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, secretKey.getBytes())
-				.compact();
+			.setSubject(username)
+			.setIssuer(issuer)
+			.setIssuedAt(new java.util.Date(System.currentTimeMillis()))
+			.setExpiration(Date.from(Instant.now().plus(15, ChronoUnit.DAYS)))
+			.signWith(io.jsonwebtoken.SignatureAlgorithm.HS512, secretKey.getBytes())
+			.compact();
 	}
 
 	/**
--- src/main/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherController.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherController.java
@@ -2,15 +2,23 @@ package org.swmaestro.repl.gifthub.vouchers.controller;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherDto;
+import org.springframework.web.multipart.MultipartFile;
+import org.swmaestro.repl.gifthub.util.JwtProvider;
+import org.swmaestro.repl.gifthub.vouchers.dto.S3FileDto;
+import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveRequestDto;
 import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveResponseDto;
+import org.swmaestro.repl.gifthub.vouchers.service.StorageService;
 import org.swmaestro.repl.gifthub.vouchers.service.VoucherService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,11 +26,24 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Tag(name = "Vouchers", description = "기프티콘 관련 API")
 public class VoucherController {
+	@Value("${cloud.aws.s3.voucher-dir-name}")
+	private String voucherDirName;
 	private final VoucherService voucherService;
+	private final StorageService storageService;
+	private final JwtProvider jwtProvider;
 
-	@PostMapping
-	public VoucherSaveResponseDto saveVoucher(@RequestBody VoucherDto voucherDto) throws IOException {
-		return voucherService.save(voucherDto);
+	@PostMapping("/image")
+	@Operation(summary = "Voucher 이미지 등록 메서드", description = "클라이언트에서 요청한 기프티콘 이미지를 Amazon S3에 저장하기 위한 메서드입니다.")
+	public S3FileDto saveVoucherImage(@RequestPart("image_file") MultipartFile imageFile) throws IOException {
+		return storageService.save(voucherDirName, imageFile);
 	}
 
+	@PostMapping
+	@Operation(summary = "Voucher 등록 메서드", description = "클라이언트에서 요청한 기프티콘 정보를 저장하기 위한 메서드입니다.")
+	public VoucherSaveResponseDto saveVoucher(HttpServletRequest request,
+		@RequestBody VoucherSaveRequestDto voucherSaveRequestDto) throws
+		IOException {
+		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
+		return voucherService.save(username, voucherSaveRequestDto);
+	}
 }
