141b1891b02c15c8379f21e7e1640f9424bcf05d
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-25 12:12:39 +0900

    feat: implement voucher-usage-history repository

---
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/repository/VoucherUsageHistoryRepository.java
@@ -0,0 +1,6 @@
+package org.swmaestro.repl.gifthub.vouchers.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoucherUsageHistoryRepository extends JpaRepository<VoucherUsageHistoryRepository, Long> {
+}
