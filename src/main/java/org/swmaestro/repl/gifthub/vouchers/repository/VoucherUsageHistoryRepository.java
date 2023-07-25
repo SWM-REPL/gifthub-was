ca2569c800348a348baca3aad4d41ec0fa77672c
Jinwoo Lee <binaryWooo@gmail.com>
2023-07-25 16:25:49 +0900

    fix: change voucher-usage-history object

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/repository/VoucherUsageHistoryRepository.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/repository/VoucherUsageHistoryRepository.java
@@ -1,6 +1,7 @@
 package org.swmaestro.repl.gifthub.vouchers.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.swmaestro.repl.gifthub.vouchers.entity.VoucherUsageHistory;
 
-public interface VoucherUsageHistoryRepository extends JpaRepository<VoucherUsageHistoryRepository, Long> {
+public interface VoucherUsageHistoryRepository extends JpaRepository<VoucherUsageHistory, Long> {
 }
