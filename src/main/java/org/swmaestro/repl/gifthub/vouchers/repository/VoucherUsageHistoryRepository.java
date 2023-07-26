7dd2071047380445f87f0ffcc7f67d71f92c83a0
inh2613 <inh2613@naver.com>
2023-07-27 01:20:37 +0900

    feat: implement find list of voucher-usage-history by voucher-id

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/repository/VoucherUsageHistoryRepository.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/repository/VoucherUsageHistoryRepository.java
@@ -3,5 +3,8 @@ package org.swmaestro.repl.gifthub.vouchers.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.swmaestro.repl.gifthub.vouchers.entity.VoucherUsageHistory;
 
+import java.util.List;
+
 public interface VoucherUsageHistoryRepository extends JpaRepository<VoucherUsageHistory, Long> {
+	List<VoucherUsageHistory> findAllByVoucherId(Long voucherId);
 }
