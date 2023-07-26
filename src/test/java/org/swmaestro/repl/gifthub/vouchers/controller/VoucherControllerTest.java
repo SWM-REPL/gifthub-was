218d94e8b418178bbd1e6ff8d702186f83d8d820
inh2613 <inh2613@naver.com>
2023-07-27 01:45:14 +0900

    test: implement voucher-use

--- src/test/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherControllerTest.java
+++ src/test/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherControllerTest.java
@@ -1,13 +1,6 @@
 package org.swmaestro.repl.gifthub.vouchers.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,13 +10,16 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.swmaestro.repl.gifthub.util.JwtProvider;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherReadResponseDto;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveRequestDto;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveResponseDto;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherUpdateRequestDto;
+import org.swmaestro.repl.gifthub.vouchers.dto.*;
 import org.swmaestro.repl.gifthub.vouchers.service.VoucherService;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -45,16 +41,16 @@ class VoucherControllerTest {
 	void saveVoucher() throws Exception {
 		// given
 		VoucherSaveRequestDto voucher = VoucherSaveRequestDto.builder()
-			.brandName("스타벅스")
-			.productName("아이스 아메리카노 T")
-			.barcode("012345678910")
-			.expiresAt("2023-06-15")
-			.imageUrl("https://s3.ap-northeast-2.amazonaws.com/gifthub-voucher/1623777600000_스타벅스_아이스아메리카노T.png")
-			.build();
+				.brandName("스타벅스")
+				.productName("아이스 아메리카노 T")
+				.barcode("012345678910")
+				.expiresAt("2023-06-15")
+				.imageUrl("https://s3.ap-northeast-2.amazonaws.com/gifthub-voucher/1623777600000_스타벅스_아이스아메리카노T.png")
+				.build();
 
 		VoucherSaveResponseDto voucherSaveResponseDto = VoucherSaveResponseDto.builder()
-			.id(1L)
-			.build();
+				.id(1L)
+				.build();
 
 		// when
 		when(jwtProvider.resolveToken(any())).thenReturn("my_awesome_access_token");
@@ -63,10 +59,10 @@ class VoucherControllerTest {
 
 		// then
 		mockMvc.perform(post("/vouchers")
-				.header("Authorization", "Bearer my_awesome_access_token")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(voucher)))
-			.andExpect(status().isOk());
+						.header("Authorization", "Bearer my_awesome_access_token")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(voucher)))
+				.andExpect(status().isOk());
 	}
 
 	/*
@@ -77,10 +73,10 @@ class VoucherControllerTest {
 		// given
 		Long voucherId = 1L;
 		VoucherReadResponseDto voucherReadResponseDto = VoucherReadResponseDto.builder()
-			.id(1L)
-			.barcode("012345678910")
-			.expiresAt("2023-06-15")
-			.build();
+				.id(1L)
+				.barcode("012345678910")
+				.expiresAt("2023-06-15")
+				.build();
 
 		when(voucherService.read(voucherId)).thenReturn(voucherReadResponseDto);
 		//when
@@ -101,24 +97,24 @@ class VoucherControllerTest {
 
 		List<VoucherReadResponseDto> voucherReadResponseDtos = new ArrayList<>();
 		voucherReadResponseDtos.add(VoucherReadResponseDto.builder()
-			.id(1L)
-			.barcode("012345678910")
-			.expiresAt("2023-06-15")
-			.build());
+				.id(1L)
+				.barcode("012345678910")
+				.expiresAt("2023-06-15")
+				.build());
 
 		voucherReadResponseDtos.add(VoucherReadResponseDto.builder()
-			.id(2L)
-			.barcode("012345678911")
-			.expiresAt("2023-06-16")
-			.build());
+				.id(2L)
+				.barcode("012345678911")
+				.expiresAt("2023-06-16")
+				.build());
 
 		when(jwtProvider.resolveToken(any())).thenReturn(accessToken);
 		when(jwtProvider.getUsername(anyString())).thenReturn(username);
 		when(voucherService.list(username)).thenReturn(voucherReadResponseDtos);
 
 		mockMvc.perform(get("/vouchers")
-				.header("Authorization", "Bearer " + accessToken))
-			.andExpect(status().isOk());
+						.header("Authorization", "Bearer " + accessToken))
+				.andExpect(status().isOk());
 	}
 
 	/*
@@ -129,15 +125,15 @@ class VoucherControllerTest {
 	void voucherUpdateTest() throws Exception {
 		// given
 		VoucherUpdateRequestDto voucherUpdateRequestDto = VoucherUpdateRequestDto.builder()
-			.brandName("스타벅스")
-			.productName("아이스 아메리카노 T")
-			.barcode("012345678910")
-			.expiresAt("2023-06-15")
-			.build();
+				.brandName("스타벅스")
+				.productName("아이스 아메리카노 T")
+				.barcode("012345678910")
+				.expiresAt("2023-06-15")
+				.build();
 
 		VoucherSaveResponseDto voucherSaveResponseDto = VoucherSaveResponseDto.builder()
-			.id(1L)
-			.build();
+				.id(1L)
+				.build();
 
 		// when
 		when(jwtProvider.resolveToken(any())).thenReturn("my_awesome_access_token");
@@ -146,9 +142,43 @@ class VoucherControllerTest {
 
 		// then
 		mockMvc.perform(patch("/vouchers/1")
-				.header("Authorization", "Bearer my_awesome_access_token")
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(voucherSaveResponseDto)))
-			.andExpect(status().isOk());
+						.header("Authorization", "Bearer my_awesome_access_token")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(voucherSaveResponseDto)))
+				.andExpect(status().isOk());
+	}
+
+	/*
+	기프티콘 사용 등록 테스트
+	 */
+	@Test
+	@WithMockUser(username = "이진우", roles = "USER")
+	void useVoucherTest() throws Exception {
+		// given
+		Long voucherId = 1L;
+		VoucherUseRequestDto voucherUseRequestDto = VoucherUseRequestDto.builder()
+				.id(1L)
+				.amount(5000)
+				.place("스타벅스 아남타워점")
+				.build();
+
+		VoucherUseResponseDto voucherUseResponseDto = VoucherUseResponseDto.builder()
+				.usageId(1L)
+				.voucherId(1L)
+				.balance(20000)
+				.build();
+
+		// when
+		when(jwtProvider.resolveToken(any())).thenReturn("my_awesome_access_token");
+		when(jwtProvider.getUsername(anyString())).thenReturn("이진우");
+		when(voucherService.use(anyString(), eq(voucherId), any(VoucherUseRequestDto.class)))
+				.thenReturn(voucherUseResponseDto);
+
+		// then
+		mockMvc.perform(post("/vouchers/1/usage")
+						.header("Authorization", "Bearer my_awesome_access_token")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(voucherUseRequestDto)))
+				.andExpect(status().isOk());
 	}
 }
