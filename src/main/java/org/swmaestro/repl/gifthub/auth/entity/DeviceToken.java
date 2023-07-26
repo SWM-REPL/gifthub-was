f1269c471e6c51ecfeabdf54a0a481d117f0c8c6
inh2613 <inh2613@naver.com>
2023-07-26 11:32:40 +0900

    fix: rename to device-token

--- src/main/java/org/swmaestro/repl/gifthub/auth/entity/DeviceToken.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/entity/DeviceToken.java
@@ -1,25 +1,18 @@
 package org.swmaestro.repl.gifthub.auth.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UserToken {
+public class DeviceToken {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -36,7 +29,7 @@ public class UserToken {
 	private LocalDateTime createdAt;
 
 	@Builder
-	public UserToken(Long id, Member member, String token, LocalDateTime createdAt) {
+	public DeviceToken(Long id, Member member, String token, LocalDateTime createdAt) {
 		this.id = id;
 		this.member = member;
 		this.token = token;
