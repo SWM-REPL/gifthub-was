f4ac3cdc7c17e200633913a10579f1704269f3e2
inh2613 <inh2613@naver.com>
2023-07-27 01:28:19 +0900

    feat: implement voucher-use-service

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/service/VoucherService.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/service/VoucherService.java
@@ -1,24 +1,24 @@
 package org.swmaestro.repl.gifthub.vouchers.service;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.swmaestro.repl.gifthub.auth.service.MemberService;
 import org.swmaestro.repl.gifthub.exception.BusinessException;
 import org.swmaestro.repl.gifthub.exception.ErrorCode;
 import org.swmaestro.repl.gifthub.util.DateConverter;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherReadResponseDto;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveRequestDto;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherSaveResponseDto;
-import org.swmaestro.repl.gifthub.vouchers.dto.VoucherUpdateRequestDto;
+import org.swmaestro.repl.gifthub.vouchers.dto.*;
 import org.swmaestro.repl.gifthub.vouchers.entity.Voucher;
+import org.swmaestro.repl.gifthub.vouchers.entity.VoucherUsageHistory;
 import org.swmaestro.repl.gifthub.vouchers.repository.VoucherRepository;
+import org.swmaestro.repl.gifthub.vouchers.repository.VoucherUsageHistoryRepository;
 
-import lombok.RequiredArgsConstructor;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,24 +30,25 @@ public class VoucherService {
 	private final ProductService productService;
 	private final VoucherRepository voucherRepository;
 	private final MemberService memberService;
+	private final VoucherUsageHistoryRepository voucherUsageHistoryRepository;
 
 	/*
 		기프티콘 저장 메서드
 	 */
 	public VoucherSaveResponseDto save(String username, VoucherSaveRequestDto voucherSaveRequestDto) throws
-		IOException {
+			IOException {
 		Voucher voucher = Voucher.builder()
-			.brand(brandService.read(voucherSaveRequestDto.getBrandName()))
-			.product(productService.read(voucherSaveRequestDto.getProductName()))
-			.barcode(voucherSaveRequestDto.getBarcode())
-			.expiresAt(DateConverter.stringToLocalDate(voucherSaveRequestDto.getExpiresAt()))
-			.imageUrl(storageService.getBucketAddress(voucherDirName) + voucherSaveRequestDto.getImageUrl())
-			.member(memberService.read(username))
-			.build();
+				.brand(brandService.read(voucherSaveRequestDto.getBrandName()))
+				.product(productService.read(voucherSaveRequestDto.getProductName()))
+				.barcode(voucherSaveRequestDto.getBarcode())
+				.expiresAt(DateConverter.stringToLocalDate(voucherSaveRequestDto.getExpiresAt()))
+				.imageUrl(storageService.getBucketAddress(voucherDirName) + voucherSaveRequestDto.getImageUrl())
+				.member(memberService.read(username))
+				.build();
 
 		return VoucherSaveResponseDto.builder()
-			.id(voucherRepository.save(voucher).getId())
-			.build();
+				.id(voucherRepository.save(voucher).getId())
+				.build();
 	}
 
 	/*
@@ -67,7 +68,7 @@ public class VoucherService {
 	사용자 별 기프티콘 목록 조회 메서드
 	 */
 	public List<VoucherReadResponseDto> list(String username) {
-		List<Voucher> vouchers = voucherRepository.findByMemberUsername(username);
+		List<Voucher> vouchers = voucherRepository.findAllByMemberUsername(username);
 		if (vouchers == null) {
 			throw new BusinessException("존재하지 않는 사용자 입니다.", ErrorCode.NOT_FOUND_RESOURCE);
 		}
@@ -83,20 +84,73 @@ public class VoucherService {
 	 */
 	public VoucherSaveResponseDto update(Long voucherId, VoucherUpdateRequestDto voucherUpdateRequestDto) {
 		Voucher voucher = voucherRepository.findById(voucherId)
-			.orElseThrow(() -> new BusinessException("존재하지 않는 상품권 입니다.", ErrorCode.NOT_FOUND_RESOURCE));
+				.orElseThrow(() -> new BusinessException("존재하지 않는 상품권 입니다.", ErrorCode.NOT_FOUND_RESOURCE));
 
 		voucher.setBarcode(
-			voucherUpdateRequestDto.getBarcode() == null ? voucher.getBarcode() : voucherUpdateRequestDto.getBarcode());
+				voucherUpdateRequestDto.getBarcode() == null ? voucher.getBarcode() : voucherUpdateRequestDto.getBarcode());
 		voucher.setBrand(voucherUpdateRequestDto.getBrandName() == null ? voucher.getBrand() :
-			brandService.read(voucherUpdateRequestDto.getBrandName()));
+				brandService.read(voucherUpdateRequestDto.getBrandName()));
 		voucher.setProduct(voucherUpdateRequestDto.getProductName() == null ? voucher.getProduct() :
-			productService.read(voucherUpdateRequestDto.getProductName()));
+				productService.read(voucherUpdateRequestDto.getProductName()));
 		voucher.setExpiresAt(voucherUpdateRequestDto.getExpiresAt() == null ? voucher.getExpiresAt() :
-			DateConverter.stringToLocalDate(voucherUpdateRequestDto.getExpiresAt()));
+				DateConverter.stringToLocalDate(voucherUpdateRequestDto.getExpiresAt()));
 
 		return VoucherSaveResponseDto.builder()
-			.id(voucherId)
-			.build();
+				.id(voucherId)
+				.build();
+	}
+
+	/*
+	기프티콘 사용 등록 메서드
+	 */
+	public VoucherUseResponseDto use(String username, Long voucherId, VoucherUseRequestDto voucherUseRequestDto) {
+		Optional<Voucher> voucher = voucherRepository.findById(voucherId);
+		List<Voucher> vouchers = voucherRepository.findAllByMemberUsername(username);
+		List<VoucherUsageHistory> voucherUsageHistories = voucherUsageHistoryRepository.findAllByVoucherId(voucherId);
+
+		if (voucher.isEmpty()) {
+			throw new BusinessException("존재하지 않는 상품권 입니다.", ErrorCode.NOT_FOUND_RESOURCE);
+		}
+		if (!vouchers.contains(voucher.get())) {
+			throw new BusinessException("상품권을 사용할 권한이 없습니다.", ErrorCode.ACCESS_DENIED);
+		}
+		int totalUsageAmount = voucherUsageHistories.stream()
+				.mapToInt(VoucherUsageHistory::getAmount)
+				.sum();
+
+		totalUsageAmount = Math.max(totalUsageAmount, 0);
+
+		if (totalUsageAmount == voucher.get().getBalance()) {
+			throw new BusinessException("이미 사용된 상품권 입니다.", ErrorCode.NOT_FOUND_RESOURCE);
+		}
+
+		int remainingBalance = voucher.get().getBalance() - totalUsageAmount;
+		int requestedAmount = voucherUseRequestDto.getAmount();
+
+		if (requestedAmount > remainingBalance) {
+			throw new BusinessException("잔액이 부족합니다.", ErrorCode.EXIST_RESOURCE);
+		}
+
+		if (voucher.get().getExpiresAt().isBefore(LocalDate.now())) {
+			throw new BusinessException("유효기간이 만료된 상품권 입니다.", ErrorCode.EXIST_RESOURCE);
+		}
+
+
+		VoucherUsageHistory voucherUsageHistory = VoucherUsageHistory.builder()
+				.member(memberService.read(username))
+				.voucher(voucher.get())
+				.amount(voucherUseRequestDto.getAmount())
+				.place(voucherUseRequestDto.getPlace())
+				.createdAt(LocalDateTime.now())
+				.build();
+
+		voucherUsageHistoryRepository.save(voucherUsageHistory);
+
+		return VoucherUseResponseDto.builder()
+				.usageId(voucherUsageHistory.getId())
+				.voucherId(voucherId)
+				.balance(remainingBalance - requestedAmount)
+				.build();
 	}
 
 	/*
@@ -104,12 +158,12 @@ public class VoucherService {
 	 */
 	public VoucherReadResponseDto mapToDto(Voucher voucher) {
 		VoucherReadResponseDto voucherReadResponseDto = VoucherReadResponseDto.builder()
-			.id(voucher.getId())
-			.barcode(voucher.getBarcode())
-			.expiresAt(voucher.getExpiresAt().toString())
-			.product(voucher.getProduct())
-			.username(voucher.getMember().getUsername())
-			.build();
+				.id(voucher.getId())
+				.barcode(voucher.getBarcode())
+				.expiresAt(voucher.getExpiresAt().toString())
+				.product(voucher.getProduct())
+				.username(voucher.getMember().getUsername())
+				.build();
 		return voucherReadResponseDto;
 	}
 }
