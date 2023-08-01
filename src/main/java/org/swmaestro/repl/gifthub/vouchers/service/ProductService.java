caf98add6d426af8170331e3a2f5479d39b4b6f6
정인희 <jeong-inhee@jeong-inhee-MacBookAir.local>
2023-08-01 15:54:00 +0900

    feat: implement product-read-method

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/service/ProductService.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/service/ProductService.java
@@ -1,6 +1,11 @@
 package org.swmaestro.repl.gifthub.vouchers.service;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
+import org.swmaestro.repl.gifthub.exception.BusinessException;
+import org.swmaestro.repl.gifthub.exception.ErrorCode;
+import org.swmaestro.repl.gifthub.vouchers.dto.ProductReadResponseDto;
 import org.swmaestro.repl.gifthub.vouchers.entity.Product;
 import org.swmaestro.repl.gifthub.vouchers.repository.ProductRepository;
 
@@ -13,5 +18,27 @@ public class ProductService {
 
 	public Product read(String productName) {
 		return productRepository.findByName(productName);
+	}
+
+	public ProductReadResponseDto readById(Long id) {
+		Optional<Product> product = productRepository.findById(id);
+		if (product.isEmpty()) {
+			throw new BusinessException("존재하지 않는 상품 입니다.", ErrorCode.NOT_FOUND_RESOURCE);
+		}
+		ProductReadResponseDto productReadResponseDto = mapToDto(product.get());
+		return productReadResponseDto;
+	}
+
+	public ProductReadResponseDto mapToDto(Product product) {
+		ProductReadResponseDto productReadResponseDto = ProductReadResponseDto.builder()
+				.id(product.getId())
+				.brand_id(product.getBrand().getId())
+				.name(product.getName())
+				.description(product.getDescription())
+				.isReusable(product.getIsReusable())
+				.price(product.getPrice())
+				.imageUrl(product.getImageUrl())
+				.build();
+		return productReadResponseDto;
 	}
 }
