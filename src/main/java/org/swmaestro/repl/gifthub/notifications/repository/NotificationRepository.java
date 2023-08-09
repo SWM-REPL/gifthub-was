4023de3d406c5aa3e0359a62812f1b2b8bd7a956
정인희 <jeong-inhee@jeong-inhee-MacBookAir.local>
2023-08-09 14:01:29 +0900

    feat: implement notification-read-response-dto

--- src/main/java/org/swmaestro/repl/gifthub/notifications/repository/NotificationRepository.java
+++ src/main/java/org/swmaestro/repl/gifthub/notifications/repository/NotificationRepository.java
@@ -1,7 +1,10 @@
 package org.swmaestro.repl.gifthub.notification.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.swmaestro.repl.gifthub.notification.entity.Notification;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	List<Notification> findAllByReceiverUsername(String username);
 }
