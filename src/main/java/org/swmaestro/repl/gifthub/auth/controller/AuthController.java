c30551389e1aadca39243467b649076c7752bb4a
Jinwoo Lee <binaryWooo@gmail.com>
2023-08-18 07:36:21 +0900

    feat: add swagger-response to auth-controller

--- src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
@@ -38,6 +38,8 @@ import org.swmaestro.repl.gifthub.util.StatusEnum;
 import com.nimbusds.jose.JOSEException;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -60,6 +62,11 @@ public class AuthController {
 
 	@PostMapping("/sign-up")
 	@Operation(summary = "회원가입 메서드", description = "사용자가 회원가입을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "회원가입 성공"),
+			@ApiResponse(responseCode = "400", description = "비밀번호 형식 부적합(영문, 숫자, 특수문자를 포함한 8자리 이상), 닉네임 글자 수 부적합(12자리 이하)"),
+			@ApiResponse(responseCode = "409", description = "이미 존재하는 아이디")
+	})
 	public ResponseEntity<Message> signUp(@RequestBody SignUpDto signUpDto) {
 		TokenDto tokenDto = memberService.create(signUpDto);
 		return new ResponseEntity<>(
@@ -75,6 +82,11 @@ public class AuthController {
 
 	@PostMapping("/sign-in")
 	@Operation(summary = "로그인 메서드", description = "사용자가 로그인을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "로그인 성공"),
+			@ApiResponse(responseCode = "400", description = "존재하지 않는 아이디, 비밀번호 불일치"),
+			@ApiResponse(responseCode = "409", description = "이미 존재하는 아이디")
+	})
 	public ResponseEntity<Message> signIn(@RequestBody SignInDto loginDto) {
 		TokenDto tokenDto = authService.signIn(loginDto);
 		return new ResponseEntity<>(
@@ -90,6 +102,10 @@ public class AuthController {
 
 	@PostMapping("/refresh")
 	@Operation(summary = "Refresh Token을 이용한 New RefreshToken, New Access Token 발급 메서드", description = "Refresh Token을 이용하여 새로운 Refresh Token, Access Token을 발급 받기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "Access Token 재발급 성공"),
+			@ApiResponse(responseCode = "400", description = "Access Token 재발급 실패"),
+	})
 	public ResponseEntity<Message> reissueAccessToken(@RequestHeader("Authorization") String refreshToken) {
 		String newAccessToken = refreshTokenService.createNewAccessTokenByValidateRefreshToken(refreshToken);
 		String newRefreshToken = refreshTokenService.createNewRefreshTokenByValidateRefreshToken(refreshToken);
@@ -121,6 +137,10 @@ public class AuthController {
 
 	@GetMapping("/sign-in/naver/callback")
 	@Operation(summary = "네이버 로그인 콜백 메서드", description = "네이버 로그인 콜백을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "네이버 로그인 성공"),
+			@ApiResponse(responseCode = "400", description = "네이버 로그인 실패"),
+	})
 	public ResponseEntity<Message> naverCallback(@RequestParam String code, @RequestParam String state) throws IOException {
 		TokenDto token = naverService.getNaverToken("token", code);
 		NaverDto naverDto = naverService.getUserInfo(token);
@@ -146,6 +166,11 @@ public class AuthController {
 
 	@GetMapping("/sign-in/kakao/callback")
 	@Operation(summary = "카카오 로그인 콜백 메서드", description = "카카오 로그인 콜백을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "카카오 로그인 성공"),
+			@ApiResponse(responseCode = "400", description = "잘못된 프로토콜 혹은 URL 요쳥"),
+			@ApiResponse(responseCode = "500", description = "HTTP 연결 수행 실패"),
+	})
 	public ResponseEntity<Message> kakaoCallback(@RequestParam String code) throws IOException {
 		TokenDto kakaoTokenDto = kakaoService.getToken(code);
 		KakaoDto kakaoDto = kakaoService.getUserInfo(kakaoTokenDto);
@@ -164,13 +189,18 @@ public class AuthController {
 	}
 
 	@GetMapping("/sign-in/google")
-	@Operation(summary = "구글 로그인 콜백 메서드", description = "구글 로그인 후 리다이렉트 되어 인가 코드를 출력하는 메서드입니다.")
+	@Operation(summary = "구글 로그인 메서드", description = "구글 로그인 후 리다이렉트 되어 인가 코드를 출력하는 메서드입니다.")
 	public void googleLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.sendRedirect(googleService.getAuthorizationUrl());
 	}
 
 	@GetMapping("/sign-in/google/callback")
-	@Operation(summary = "구글 로그인 메서드", description = "구글로부터 사용자 정보를 얻어와 회원가입 및 로그인을 하기 위한 메서드입니다.")
+	@Operation(summary = "구글 로그인 콜백 메서드", description = "구글로부터 사용자 정보를 얻어와 회원가입 및 로그인을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "구글 로그인 성공"),
+			@ApiResponse(responseCode = "400", description = "잘못된 프로토콜 혹은 URL 요쳥"),
+			@ApiResponse(responseCode = "500", description = "HTTP 연결 수행 실패"),
+	})
 	public ResponseEntity<Message> signIn(@RequestParam String code) throws IOException {
 		TokenDto googleTokenDto = googleService.getToken(code);
 		GoogleDto googleDto = googleService.getUserInfo(googleTokenDto);
@@ -196,6 +226,10 @@ public class AuthController {
 
 	@PostMapping("/sign-in/apple/callback")
 	@Operation(summary = "애플 로그인 콜백 메서드", description = "애플 로그인 콜백을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "애플 로그인 성공"),
+			@ApiResponse(responseCode = "400", description = "애플 로그인 실패"),
+	})
 	public ResponseEntity<Message> appleCallback(@RequestBody String code) throws IOException, ParseException, JOSEException {
 		String keyPath = appleService.readKeyPath();
 		PrivateKey privateKey = appleService.craetePrivateKey(keyPath);
@@ -218,6 +252,10 @@ public class AuthController {
 
 	@PostMapping("/sign-out")
 	@Operation(summary = "로그아웃 메서드", description = "사용자가 로그아웃을 하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "로그인 성공"),
+			@ApiResponse(responseCode = "401", description = "존재하지 않는 사용자")
+	})
 	public ResponseEntity<Message> signOut(HttpServletRequest request) {
 		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
 		authService.signOut(username);
