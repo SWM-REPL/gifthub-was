cce4bbaf565fe36944e51db917b598d4fc967e23
Jinwoo Lee <binaryWooo@gmail.com>
2023-08-18 22:26:15 +0900

    feat: add swagger-response to voucher-controller

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherController.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/controller/VoucherController.java
@@ -26,6 +26,8 @@ import org.swmaestro.repl.gifthub.vouchers.service.StorageService;
 import org.swmaestro.repl.gifthub.vouchers.service.VoucherService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -43,6 +45,10 @@ public class VoucherController {
 
 	@PostMapping("/images")
 	@Operation(summary = "Voucher 이미지 등록 메서드", description = "클라이언트에서 요청한 기프티콘 이미지를 Amazon S3에 저장하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "기프티콘 이미지 등록 성공"),
+			@ApiResponse(responseCode = "400(404)", description = "존재하지 않는 브랜드 조회 시도")
+	})
 	public ResponseEntity<Message> saveVoucherImage(@RequestPart("image_file") MultipartFile imageFile) throws IOException {
 		return new ResponseEntity<>(
 				Message.builder()
@@ -57,6 +63,11 @@ public class VoucherController {
 
 	@PostMapping
 	@Operation(summary = "Voucher 등록 메서드", description = "클라이언트에서 요청한 기프티콘 정보를 저장하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "기프티콘 등록 성공"),
+			@ApiResponse(responseCode = "400(404-1)", description = "존재하지 않는 브랜드 입력"),
+			@ApiResponse(responseCode = "400(404-2)", description = "존재하지 않는 상품 입력"),
+	})
 	public ResponseEntity<Message> saveVoucher(HttpServletRequest request,
 			@RequestBody VoucherSaveRequestDto voucherSaveRequestDto) throws
 			IOException {
@@ -74,6 +85,11 @@ public class VoucherController {
 
 	@GetMapping("/{voucherId}")
 	@Operation(summary = "Voucher 상세 조회 메서드", description = "클라이언트에서 요청한 기프티콘 상세 정보를 조회하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "기프티콘 상세 조회 성공"),
+			@ApiResponse(responseCode = "400(403)", description = "자신의 것이 아닌 기프티콘 조회 시도"),
+			@ApiResponse(responseCode = "400(404)", description = "존재하지 않는 기프티콘 조회 시도"),
+	})
 	public ResponseEntity<Message> readVoucher(HttpServletRequest request, @PathVariable Long voucherId) throws
 			IOException {
 		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
@@ -90,6 +106,10 @@ public class VoucherController {
 
 	@GetMapping
 	@Operation(summary = "Voucher 목록 조회 메서드", description = "클라이언트에서 요청한 사용자 별 기프티콘 목록 정보를 조회하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "기프티콘 목록 조회 성공"),
+			@ApiResponse(responseCode = "400(403)", description = "자신의 것이 아닌 기프티콘 조회 시도"),
+	})
 	public ResponseEntity<Message> listVoucher(HttpServletRequest request, @RequestParam(value = "user_id", required = true) Long memberId) {
 		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
 		return new ResponseEntity<>(
@@ -105,6 +125,10 @@ public class VoucherController {
 
 	@PatchMapping("/{voucherId}")
 	@Operation(summary = "Voucher 수정 메서드", description = "클라이언트에서 요청한 기프티콘 정보를 수정하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "기프티콘 수정 성공"),
+			@ApiResponse(responseCode = "400(404)", description = "존재하지 않는 기프티콘 조회 시도"),
+	})
 	public ResponseEntity<Message> updateVoucher(@PathVariable Long voucherId,
 			@RequestBody VoucherUpdateRequestDto voucherUpdateRequestDto) throws IOException {
 		return new ResponseEntity<>(
@@ -120,6 +144,14 @@ public class VoucherController {
 
 	@PostMapping("/{voucherId}/usage")
 	@Operation(summary = "Voucher 사용 메서드", description = "클라이언트에서 요청한 기프티콘 사용 정보를 저장하기 위한 메서드입니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "기프티콘 사용 히스토리 등록 성공"),
+			@ApiResponse(responseCode = "400(400-1)", description = "유효하지 않은 사용 금액 입력"),
+			@ApiResponse(responseCode = "400(400-2)", description = "사용처 미입력"),
+			@ApiResponse(responseCode = "400(403)", description = "상품권 사용 권한 없음"),
+			@ApiResponse(responseCode = "400(404-1)", description = "존재하지 않는 상품권 사용 시도"),
+			@ApiResponse(responseCode = "400(404-2)", description = "이미 사용한 상품권 사용 ㅅ도"),
+	})
 	public ResponseEntity<Message> useVoucher(HttpServletRequest request, @PathVariable Long voucherId,
 			@RequestBody VoucherUseRequestDto voucherUseRequestDto) throws IOException {
 		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
