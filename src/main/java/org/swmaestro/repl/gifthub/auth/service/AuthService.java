564ba9be9dd503234960c3751d2f4ba4f40eb377
Jinwoo Lee <binaryWooo@gmail.com>
2023-09-18 10:15:15 +0900

    fix: add device-token-deletion logic

--- src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthService.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthService.java
@@ -1,11 +1,12 @@
 package org.swmaestro.repl.gifthub.auth.service;
 
 import org.swmaestro.repl.gifthub.auth.dto.SignInDto;
+import org.swmaestro.repl.gifthub.auth.dto.SignOutDto;
 import org.swmaestro.repl.gifthub.auth.dto.TokenDto;
 
 public interface AuthService {
 
 	TokenDto signIn(SignInDto loginDto);
 
-	void signOut(String username);
+	void signOut(String username, SignOutDto signOutDto);
 }
--- src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/AuthServiceImpl.java
@@ -3,6 +3,7 @@ package org.swmaestro.repl.gifthub.auth.service;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.swmaestro.repl.gifthub.auth.dto.SignInDto;
+import org.swmaestro.repl.gifthub.auth.dto.SignOutDto;
 import org.swmaestro.repl.gifthub.auth.dto.TokenDto;
 import org.swmaestro.repl.gifthub.auth.entity.Member;
 import org.swmaestro.repl.gifthub.auth.repository.MemberRepository;
@@ -21,6 +22,7 @@ public class AuthServiceImpl implements AuthService {
 	private final JwtProvider jwtProvider;
 	private final RefreshTokenService refreshTokenService;
 	private final NaverService naverService;
+	private final DeviceTokenService deviceTokenService;
 
 	public TokenDto signIn(SignInDto loginDto) {
 		Member member = memberRepository.findByUsername(loginDto.getUsername());
@@ -44,11 +46,12 @@ public class AuthServiceImpl implements AuthService {
 	}
 
 	@Transactional
-	public void signOut(String username) {
+	public void signOut(String username, SignOutDto signOutDto) {
 		Member member = memberRepository.findByUsername(username);
 		if (member == null) {
 			throw new BusinessException("존재하지 않는 사용자입니다.", StatusEnum.UNAUTHORIZED);
 		}
 		refreshTokenService.deleteRefreshToken(username);
+		deviceTokenService.delete(signOutDto.getDeviceToken());
 	}
 }
