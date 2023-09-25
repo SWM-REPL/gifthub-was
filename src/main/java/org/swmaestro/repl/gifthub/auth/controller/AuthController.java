4ac3d13154067c58ebc9dc9864854b8f52136cc1
Jinwoo Lee <binaryWooo@gmail.com>
2023-09-26 03:01:05 +0900

    fix: delete redirect google login-page

--- src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
@@ -185,12 +185,6 @@ public class AuthController {
 		);
 	}
 
-	@GetMapping("/sign-in/google")
-	@Operation(summary = "구글 로그인 메서드", description = "구글 로그인 후 리다이렉트 되어 인가 코드를 출력하는 메서드입니다.")
-	public void googleLogin(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		response.sendRedirect(googleService.getAuthorizationUrl());
-	}
-
 	@GetMapping("/sign-in/google/callback")
 	@Operation(summary = "구글 로그인 콜백 메서드", description = "구글로부터 사용자 정보를 얻어와 회원가입 및 로그인을 하기 위한 메서드입니다.")
 	@ApiResponses({
