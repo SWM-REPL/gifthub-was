b5760ded3a19c0a7d5013a60aa60e719a03cee05
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-05 06:16:39 +0900

    fix: modify security exception-handling

--- src/main/java/org/swmaestro/repl/gifthub/config/SecurityConfig.java
+++ src/main/java/org/swmaestro/repl/gifthub/config/SecurityConfig.java
@@ -15,9 +15,12 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.swmaestro.repl.gifthub.filter.JwtAuthenticationFilter;
+import org.swmaestro.repl.gifthub.util.Message;
+import org.swmaestro.repl.gifthub.util.StatusEnum;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.sentry.Sentry;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -27,6 +30,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class SecurityConfig {
 	private final JwtAuthenticationFilter jwtAuthenticationFilter;
+	private final ObjectMapper objectMapper = new ObjectMapper();
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -48,27 +52,31 @@ public class SecurityConfig {
 						.accessDeniedHandler(new AccessDeniedHandler() {
 							@Override
 							public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws
-									IOException,
-									ServletException,
 									IOException {
 								// 권한 문제가 발생했을 때 이 부분을 호출한다.
 								response.setStatus(403);
 								response.setCharacterEncoding("utf-8");
-								response.setContentType("text/html; charset=UTF-8");
-								response.getWriter().write("권한이 없습니다.");
-								Sentry.captureException(accessDeniedException);
+								response.setContentType("application/json");
+								response.getWriter().write(objectMapper.writeValueAsString(
+										Message.builder()
+												.status(StatusEnum.FORBIDDEN)
+												.message("권한이 없습니다.")
+												.build()));
 							}
 						})
 						.authenticationEntryPoint(new AuthenticationEntryPoint() {
 							@Override
 							public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws
-									IOException,
-									ServletException {
+									IOException {
 								// 인증문제가 발생했을 때 이 부분을 호출한다.
 								response.setStatus(401);
 								response.setCharacterEncoding("utf-8");
-								response.setContentType("text/html; charset=UTF-8");
-								response.getWriter().write("인증되지 않은 사용자입니다.");
+								response.setContentType("application/json");
+								response.getWriter().write(objectMapper.writeValueAsString(
+										Message.builder()
+												.status(StatusEnum.FORBIDDEN)
+												.message("인증되지 않은 사용자입니다.")
+												.build()));
 								Sentry.captureException(authException);
 							}
 						})
