b255caf5574d898947d8b3e24510f895008cefd9
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-05 10:00:20 +0900

    feat: add notice-type to notification-type

--- src/main/java/org/swmaestro/repl/gifthub/notifications/NotificationType.java
+++ src/main/java/org/swmaestro/repl/gifthub/notifications/NotificationType.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 @Getter
 public enum NotificationType {
 	EXPIRATION(0, "유효기간 임박 알림"),     // 유효기간 임박
-	RECOMMENDATION(1, "사용 추천 알림");   // 사용 추천
+	// RECOMMENDATION(1, "사용 추천 알림"),   // 사용 추천
+	NOTICE(1, "공지사항 알림");           // 공지사항
 
 	private final int value;
 	private final String description;
