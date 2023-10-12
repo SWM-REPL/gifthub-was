975efae5ff31d3b601eaafbe279108701de2e5ff
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-12 13:42:57 +0900

    feat: implement create oauth-info api

--- src/main/java/org/swmaestro/repl/gifthub/auth/controller/UserController.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/controller/UserController.java
@@ -5,14 +5,21 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.swmaestro.repl.gifthub.auth.dto.MemberDeleteResponseDto;
 import org.swmaestro.repl.gifthub.auth.dto.MemberUpdateRequestDto;
+import org.swmaestro.repl.gifthub.auth.dto.OAuthTokenDto;
+import org.swmaestro.repl.gifthub.auth.entity.Member;
 import org.swmaestro.repl.gifthub.auth.service.MemberService;
+import org.swmaestro.repl.gifthub.auth.service.OAuthService;
+import org.swmaestro.repl.gifthub.auth.type.OAuthPlatform;
+import org.swmaestro.repl.gifthub.exception.BusinessException;
 import org.swmaestro.repl.gifthub.util.JwtProvider;
 import org.swmaestro.repl.gifthub.util.Message;
+import org.swmaestro.repl.gifthub.util.StatusEnum;
 import org.swmaestro.repl.gifthub.util.SuccessMessage;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,6 +35,7 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "Users", description = "사용자 관련 API")
 public class UserController {
 	private final MemberService memberService;
+	private final OAuthService oAuthService;
 	private final JwtProvider jwtProvider;
 
 	@DeleteMapping("/{userId}")
@@ -74,5 +82,31 @@ public class UserController {
 						.path(request.getRequestURI())
 						.data(memberService.read(userId))
 						.build());
+	}
+
+	@PostMapping("/oauth/{platform}")
+	@Operation(summary = "OAuth 연동 계정 추가 메서드", description = "사용자의 기존 계정에 OAuth 계정을 추가 연동하기 위한 메서드입니다.\n 파라미터로는 'naver', 'kakao', 'google', 'apple'를 받을 수 있습니다.")
+	@ApiResponses({
+			@ApiResponse(responseCode = "200", description = "OAuth 연동 계정 추가 성공"),
+			@ApiResponse(responseCode = "400", description = "OAuth 연동 계정 추가 실패")
+	})
+	public ResponseEntity<Message> createOAuthInfo(HttpServletRequest request, @PathVariable String platform,
+			@RequestBody OAuthTokenDto oAuthTokenDto) {
+		OAuthPlatform oAuthPlatform;
+
+		switch (platform) {
+			case "naver" -> oAuthPlatform = OAuthPlatform.NAVER;
+			case "kakao" -> oAuthPlatform = OAuthPlatform.KAKAO;
+			case "google" -> oAuthPlatform = OAuthPlatform.GOOGLE;
+			case "apple" -> oAuthPlatform = OAuthPlatform.APPLE;
+			default -> throw new BusinessException("올바르지 않은 플랫폼에 대한 요청입니다.", StatusEnum.BAD_REQUEST);
+		}
+
+		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
+		Member member = memberService.read(username);
+		memberService.createOAuthInfo(member, oAuthPlatform, oAuthTokenDto);
+		return ResponseEntity.ok(SuccessMessage.builder()
+				.path(request.getRequestURI())
+				.build());
 	}
 }
