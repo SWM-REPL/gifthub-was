2332a40408b5c8fef5ad57d1f191808f33ec65df
Jeong-In-Hee <inh2613@naver.com>
2023-10-13 09:44:40 +0900

    fix: change int to integer to nullable

--- src/main/java/org/swmaestro/repl/gifthub/vouchers/entity/VoucherUsageHistory.java
+++ src/main/java/org/swmaestro/repl/gifthub/vouchers/entity/VoucherUsageHistory.java
@@ -3,10 +3,12 @@ package org.swmaestro.repl.gifthub.vouchers.entity;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.swmaestro.repl.gifthub.auth.entity.Member;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -19,6 +21,7 @@ import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class VoucherUsageHistory {
 	@Id
@@ -33,7 +36,8 @@ public class VoucherUsageHistory {
 	@JoinColumn(name = "voucher_id", nullable = false)
 	private Voucher voucher;
 
-	private int amount;
+	@Column(nullable = true)
+	private Integer amount;
 
 	private String place;
 
@@ -42,8 +46,8 @@ public class VoucherUsageHistory {
 	private LocalDateTime createdAt;
 
 	@Builder
-	public VoucherUsageHistory(Long id, Member member, Voucher voucher, int amount, String place,
-		LocalDateTime createdAt) {
+	public VoucherUsageHistory(Long id, Member member, Voucher voucher, Integer amount, String place,
+			LocalDateTime createdAt) {
 		this.id = id;
 		this.member = member;
 		this.voucher = voucher;
