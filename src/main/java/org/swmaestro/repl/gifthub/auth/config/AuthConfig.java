3681f956010b498a608a8bb990ba65dd226fdb5a
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-13 15:15:26 +0900

    fix: modify kakao oauth

--- src/main/java/org/swmaestro/repl/gifthub/auth/config/AuthConfig.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/config/AuthConfig.java
@@ -12,4 +12,5 @@ import lombok.Setter;
 @Setter
 public class AuthConfig {
 	private String defaultNickname;
+	private String defaultPassword;
 }
--- src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/controller/AuthController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.swmaestro.repl.gifthub.auth.dto.JwtTokenDto;
 import org.swmaestro.repl.gifthub.auth.dto.OAuthTokenDto;
 import org.swmaestro.repl.gifthub.auth.dto.SignInDto;
-import org.swmaestro.repl.gifthub.auth.dto.SignOutDto;
 import org.swmaestro.repl.gifthub.auth.dto.SignUpDto;
 import org.swmaestro.repl.gifthub.auth.service.AuthService;
 import org.swmaestro.repl.gifthub.auth.service.RefreshTokenService;
@@ -162,13 +161,12 @@ public class AuthController {
 			@ApiResponse(responseCode = "200", description = "로그인 성공"),
 			@ApiResponse(responseCode = "400(401)", description = "존재하지 않는 사용자")
 	})
-	public ResponseEntity<Message> signOut(HttpServletRequest request, @RequestBody SignOutDto signOutDto) {
+	public ResponseEntity<Message> signOut(HttpServletRequest request) {
 		String username = jwtProvider.getUsername(jwtProvider.resolveToken(request).substring(7));
-		authService.signOut(username, signOutDto);
+		authService.signOut(username);
 		return ResponseEntity.ok(
 				SuccessMessage.builder()
 						.path(request.getRequestURI())
-						.data(null)
 						.build());
 	}
 }
