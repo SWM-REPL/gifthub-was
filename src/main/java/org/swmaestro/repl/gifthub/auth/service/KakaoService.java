930be7f1e8d361e6d5b9e8c522c7a644332c6008
Jeong-In-Hee <inh2613@naver.com>
2023-10-18 17:54:27 +0900

    fix: modify fix is-exists method

--- src/main/java/org/swmaestro/repl/gifthub/auth/service/KakaoService.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/KakaoService.java
@@ -98,10 +98,10 @@ public class KakaoService implements OAuth2Service {
 
 	@Override
 	public OAuth read(OAuthUserInfoDto oAuthUserInfoDto) {
-		OAuth oAuth = oAuthRepository.findByPlatformAndPlatformId(OAuthPlatform.KAKAO, oAuthUserInfoDto.getId())
+		OAuth oAuth = oAuthRepository.findByPlatformAndPlatformIdAndDeletedAtIsNull(OAuthPlatform.KAKAO, oAuthUserInfoDto.getId())
 				.orElseThrow(() -> new BusinessException("존재하지 않는 OAuth 계정입니다.", StatusEnum.NOT_FOUND));
 
-		if (oAuth.getDeletedAt() != null) {
+		if (oAuth.isDeleted()) {
 			throw new BusinessException("존재하지 않는 OAuth 계정입니다.", StatusEnum.NOT_FOUND);
 		} else {
 			return oAuth;
@@ -119,12 +119,15 @@ public class KakaoService implements OAuth2Service {
 
 	@Override
 	public boolean isExists(User user) {
+		if (!user.isEnabled()) {
+			throw new BusinessException("탈퇴한 회원입니다.", StatusEnum.BAD_REQUEST);
+		}
 		return oAuthRepository.findByUserAndPlatform(user, OAuthPlatform.KAKAO).isPresent();
 	}
 
 	@Override
 	public boolean isExists(OAuthUserInfoDto oAuthUserInfoDto) {
-		return oAuthRepository.findByPlatformAndPlatformId(OAuthPlatform.KAKAO, oAuthUserInfoDto.getId()).isPresent();
+		return oAuthRepository.findByPlatformAndPlatformIdAndDeletedAtIsNull(OAuthPlatform.KAKAO, oAuthUserInfoDto.getId()).isPresent();
 	}
 
 	private String getStringOrNull(JsonElement element, String fieldName) {
--- src/main/java/org/swmaestro/repl/gifthub/auth/service/NaverService.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/service/NaverService.java
@@ -103,7 +103,7 @@ public class NaverService implements OAuth2Service {
 
 	@Override
 	public OAuth read(OAuthUserInfoDto oAuthUserInfoDto) {
-		OAuth oAuth = oAuthRepository.findByPlatformAndPlatformId(OAuthPlatform.NAVER, oAuthUserInfoDto.getId())
+		OAuth oAuth = oAuthRepository.findByPlatformAndPlatformIdAndDeletedAtIsNull(OAuthPlatform.NAVER, oAuthUserInfoDto.getId())
 				.orElseThrow(() -> new BusinessException("존재하지 않는 OAuth 계정입니다.", StatusEnum.NOT_FOUND));
 
 		if (oAuth.getDeletedAt() != null) {
@@ -124,12 +124,15 @@ public class NaverService implements OAuth2Service {
 
 	@Override
 	public boolean isExists(User user) {
+		if (!user.isEnabled()) {
+			throw new BusinessException("탈퇴한 회원입니다.", StatusEnum.BAD_REQUEST);
+		}
 		return oAuthRepository.findByUserAndPlatform(user, OAuthPlatform.NAVER).isPresent();
 	}
 
 	@Override
 	public boolean isExists(OAuthUserInfoDto oAuthUserInfoDto) {
-		return oAuthRepository.findByPlatformAndPlatformId(OAuthPlatform.NAVER, oAuthUserInfoDto.getId()).isPresent();
+		return oAuthRepository.findByPlatformAndPlatformIdAndDeletedAtIsNull(OAuthPlatform.NAVER, oAuthUserInfoDto.getId()).isPresent();
 	}
 
 	private String getStringOrNull(JsonElement element, String fieldName) {
