5b4228dbc1a4345fb9556e8e696996a202f93cb3
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-19 15:50:51 +0900

    feat: add member-filed

--- src/main/java/org/swmaestro/repl/gifthub/giftcard/config/GiftcardConfig.java
+++ src/main/java/org/swmaestro/repl/gifthub/giftcard/config/GiftcardConfig.java
@@ -1,6 +1,8 @@
 package org.swmaestro.repl.gifthub.giftcard.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.stereotype.Component;
 
 import lombok.Getter;
@@ -12,4 +14,11 @@ import lombok.Setter;
 @Setter
 public class GiftcardConfig {
 	private int effectiveDay;
+	private String secret;
+	private String salt;
+
+	@Bean
+	public AesBytesEncryptor aesBytesEncryptor() {
+		return new AesBytesEncryptor(secret, salt);
+	}
 }
