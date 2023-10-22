79624c48184c10c0da8e13d6737ced9a4074f9cc
Jeong-In-Hee <inh2613@naver.com>
2023-10-22 19:41:44 +0900

    feat: declare find-by-username where deleted-at is null

--- src/main/java/org/swmaestro/repl/gifthub/auth/repository/UserRepository.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/repository/UserRepository.java
@@ -8,4 +8,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUsername(String username);
 
 	User findByNickname(String nickname);
+
+	User findByUsernameAndDeletedAtIsNull(String username);
 }
