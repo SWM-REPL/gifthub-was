49ad1b5c625f43564c22d76392ec26f5b3ea61b8
Jinwoo Lee <binaryWooo@gmail.com>
2023-10-23 19:01:52 +0900

    fix: add no-args-constructor annotation

--- src/main/java/org/swmaestro/repl/gifthub/auth/dto/UserUpdateRequestDto.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/dto/UserUpdateRequestDto.java
@@ -3,10 +3,13 @@ package org.swmaestro.repl.gifthub.auth.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UserUpdateRequestDto {
 	private String nickname;
