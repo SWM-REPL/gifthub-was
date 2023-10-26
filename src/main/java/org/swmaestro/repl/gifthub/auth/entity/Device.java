524daaf4a5fa332d1bc1f9ae39097776f1a09e21
Jeong-In-Hee <inh2613@naver.com>
2023-10-26 22:04:55 +0900

    feat: implement to determine refresh-token

--- src/main/java/org/swmaestro/repl/gifthub/auth/entity/Device.java
+++ src/main/java/org/swmaestro/repl/gifthub/auth/entity/Device.java
@@ -1,37 +1,52 @@
 package org.swmaestro.repl.gifthub.auth.entity;
 
-import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class RefreshToken {
+@EntityListeners(AuditingEntityListener.class)
+public class Device {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(nullable = false)
-	private String token;
+	private Long userId;
 
-	@Column(length = 60, nullable = false)
-	private String username;
+	@Column(length = 255, nullable = false)
+	private String refreshToken;
+
+	@Column(length = 200, nullable = false)
+	private String deviceToken;
+
+	@Column(length = 200)
+	private String fcmToken;
 
 	@CreatedDate
 	@Column(nullable = false)
 	private LocalDateTime createdAt;
 
 	@Builder
-	public RefreshToken(Long id, String token, String username, LocalDateTime createdAt) {
+	public Device(Long id, Long userId, String refreshToken, String deviceToken, String fcmToken) {
 		this.id = id;
-		this.token = token;
-		this.username = username;
-		this.createdAt = createdAt;
+		this.userId = userId;
+		this.refreshToken = refreshToken;
+		this.deviceToken = deviceToken;
+		this.fcmToken = fcmToken;
 	}
 }
