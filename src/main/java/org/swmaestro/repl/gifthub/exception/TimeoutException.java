6a34de238a911286ad4ae3602743d65ebd9613b3
Jeong-In-Hee <inh2613@naver.com>
2023-11-02 02:32:08 +0900

    feat: implement time-out exception

--- src/main/java/org/swmaestro/repl/gifthub/exception/TimeoutException.java
+++ src/main/java/org/swmaestro/repl/gifthub/exception/TimeoutException.java
@@ -1,2 +1,12 @@
-package org.swmaestro.repl.gifthub.exception;public class TimeoutException {
+package org.swmaestro.repl.gifthub.exception;
+
+import org.swmaestro.repl.gifthub.util.StatusEnum;
+
+public class TimeoutException extends RuntimeException {
+	private StatusEnum status;
+
+	public TimeoutException(String message, StatusEnum status) {
+		super(message);
+		this.status = status;
+	}
 }
