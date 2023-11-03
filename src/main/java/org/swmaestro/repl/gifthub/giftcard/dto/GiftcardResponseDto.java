343d1a598ad22aa094554b7b4760a10ab24f3ea5
Jeong-In-Hee <inh2613@naver.com>
2023-11-03 12:08:34 +0900

    fix: change product-brand field to dto

--- src/main/java/org/swmaestro/repl/gifthub/giftcard/dto/GiftcardResponseDto.java
+++ src/main/java/org/swmaestro/repl/gifthub/giftcard/dto/GiftcardResponseDto.java
@@ -2,8 +2,8 @@ package org.swmaestro.repl.gifthub.giftcard.dto;
 
 import java.time.LocalDate;
 
-import org.swmaestro.repl.gifthub.vouchers.entity.Brand;
-import org.swmaestro.repl.gifthub.vouchers.entity.Product;
+import org.swmaestro.repl.gifthub.vouchers.dto.BrandReadResponseDto;
+import org.swmaestro.repl.gifthub.vouchers.dto.ProductReadResponseDto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -18,12 +18,12 @@ import lombok.Setter;
 public class GiftcardResponseDto {
 	private String sender;
 	private String message;
-	private Brand brand;
-	private Product product;
+	private BrandReadResponseDto brand;
+	private ProductReadResponseDto product;
 	private LocalDate expiresAt;
 
 	@Builder
-	public GiftcardResponseDto(String sender, String message, Brand brand, Product product, LocalDate expiresAt) {
+	public GiftcardResponseDto(String sender, String message, BrandReadResponseDto brand, ProductReadResponseDto product, LocalDate expiresAt) {
 		this.sender = sender;
 		this.message = message;
 		this.brand = brand;
