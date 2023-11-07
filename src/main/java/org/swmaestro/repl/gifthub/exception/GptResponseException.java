6030fa2e1b2ec8ee3514f34494988287a027fdc8
Jeong-In-Hee <inh2613@naver.com>
2023-11-07 14:32:31 +0900

    fix: collect sentry error when exception occurred

--- src/main/java/org/swmaestro/repl/gifthub/exception/GptResponseException.java
+++ src/main/java/org/swmaestro/repl/gifthub/exception/GptResponseException.java
@@ -2,11 +2,19 @@ package org.swmaestro.repl.gifthub.exception;
 
 import org.swmaestro.repl.gifthub.util.StatusEnum;
 
+import io.sentry.Sentry;
+
 public class GptResponseException extends RuntimeException {
 	private StatusEnum status;
 
 	public GptResponseException(String message, StatusEnum status) {
 		super(message);
 		this.status = status;
+		captureExceptionWithSentry(this);
+	}
+
+	private void captureExceptionWithSentry(Throwable throwable) {
+		Sentry.captureException(throwable);
+
 	}
 }
--- src/main/java/org/swmaestro/repl/gifthub/exception/TimeoutException.java
+++ src/main/java/org/swmaestro/repl/gifthub/exception/TimeoutException.java
@@ -2,11 +2,19 @@ package org.swmaestro.repl.gifthub.exception;
 
 import org.swmaestro.repl.gifthub.util.StatusEnum;
 
+import io.sentry.Sentry;
+
 public class TimeoutException extends RuntimeException {
 	private StatusEnum status;
 
 	public TimeoutException(String message, StatusEnum status) {
 		super(message);
 		this.status = status;
+		captureExceptionWithSentry(this);
+	}
+
+	private void captureExceptionWithSentry(Throwable throwable) {
+		Sentry.captureException(throwable);
+
 	}
 }
